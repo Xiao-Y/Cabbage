9e3d0458044b0a12fedd2051ce92a207ac8968f9
Xiao-Y <lyongtao123@126.com>
2020-08-21 09:03:32 +0800

    添加说明信息

--- custom-test/src/main/java/com/billow/test/CabbageApp.java
+++ custom-test/src/main/java/com/billow/test/CabbageApp.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
+ * 启动
+ *
  * @author liuyongtao
  * @create 2020-08-20 14:35
  */
